ea44519b56b778923d376de106466eb2bf37821b
BlackbirdBlina <sabrinapop_rn@hotmail.com>
2025-05-18 14:02:34 -0300

    Deixando a assinatura das funções no padrão de C++ e incluindo as funções de ordenar e contagem de especialidades distintas

--- Unidade 1/Exercício4/include_cpp/busca_seq_ordenada.hpp
+++ Unidade 1/Exercício4/include_cpp/busca_seq_ordenada.hpp
@@ -2,4 +2,11 @@
 #include <vector>
 using namespace std;
 
-int busca_seq_ordenada(const vector<int>& arr, int alvo);
+void ordenar(vector<int> &vetor, int tamanho);
+
+int busca_seq_ordenada(const vector<int> &vetor, int n, int alvo);
+
+int conta_especialidades_distintas(vector<int> &arr, int n);
+
+// Essa função foi usada apenas para verificar a saída do vetor, para identificar se estava ordenando corretamente
+// void exibir_vetor(vector<int> &vetor, int tamanho);
