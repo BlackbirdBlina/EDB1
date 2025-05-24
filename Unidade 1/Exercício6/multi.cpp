ae06e7181645e3eae0bd53cd91a2e9b488de7eda
BlackbirdBlina <sabrinapop_rn@hotmail.com>
2025-05-24 19:34:43 -0300

    Corrigindo arquivo de implemenação

--- Unidade 1/Exercício6/multi.cpp
+++ Unidade 1/Exercício6/multi.cpp
@@ -11,18 +11,3 @@ int multi(int a, int b){
         return multi(a, b - 1) + a;
     }
 }
-
-
-#include "multi.hpp"
-
-// Função que realiza multiplicação através de somas sucessivas de modo recursivo
-int div(int a, int b){
-
-    // Caso base
-    if (a < b){
-        return 0;
-    } else {
-
-        return div(a-b, b) + 1;
-    }
-}
