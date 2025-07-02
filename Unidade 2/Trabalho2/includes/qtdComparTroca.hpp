79a6a8b0574e4ecbe4e730d0097f5e2dc94ea43c
BlackbirdBlina <sabrinapop_rn@hotmail.com>
2025-07-01 21:51:59 -0300

    Modificações feitas para permitir preencher o vetor com outros tipos de dados

--- Unidade 2/Trabalho2/includes/qtdComparTroca.hpp
+++ Unidade 2/Trabalho2/includes/qtdComparTroca.hpp
@@ -2,6 +2,11 @@
 #define qtdComparTroca_hpp
 using namespace std;
 
+/* Artifício utilizado para flexibilizar o uso de tipos distintos no preenchimento do vetor
+    Para alterar o tipo de dado dos valores do vetor basta trocar o tipo nessa definição */
+typedef int TipoAlvo;
+
+// Capturar e retorna um conjunto de informações
 struct ContaComparEtrocas{
     int qtdComparacoes;
     int qtdTrocas;
