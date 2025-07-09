9253c0fe9cf5f1146b8d831cdcae1917fbc8cce7
BlackbirdBlina <sabrinapop_rn@hotmail.com>
2025-07-09 13:19:57 -0300

    Realizando otimização

--- Unidade 2/Binary Insertion Sort/src/binaryInsertionSort.cpp
+++ Unidade 2/Binary Insertion Sort/src/binaryInsertionSort.cpp
@@ -1,4 +1,6 @@
 #include <vector>
+#include <cstring>
+#include <iostream>
 #include "binaryInsertionSort.hpp"
 #include "qtdComparTroca.hpp"
 using namespace std;
@@ -29,15 +31,19 @@ ContaComparEtrocas binaryInsertionSort(vector<TipoAlvo> &v, int tamanho){
         int elemento_ordenado = elemento_n_ordenado - 1;
         int indice = buscaBinaria(v, elemento_n_ordenado, aux, contagem);
 
-        // Caso queira ordenar de forma decrescente modifique o operador da segunda condição do while para "<"
-        for (int i = indice; i < elemento_n_ordenado; ++i){
+        // Cria um bloco que otimiza os deslocamentos, não sendo necessário deslocar elemento por elemento
+        memmove(v.data() + indice + 1, v.data() + indice, (elemento_n_ordenado - indice)*sizeof(TipoAlvo));
 
-            v[elemento_ordenado + 1] = v[elemento_ordenado];
-            // Extra - Considerando "mover dados" como trocas
-            ++contagem.qtdTrocas;
-            elemento_ordenado -= 1;
-        }
-        ++contagem.qtdComparacoes;
+        ++contagem.qtdTrocas;
+        // Caso queira ordenar de forma decrescente modifique o operador da segunda condição do while para "<"
+        // for (int i = indice; i < elemento_n_ordenado; ++i){
+
+        //     v[elemento_ordenado + 1] = v[elemento_ordenado];
+        //     // Extra - Considerando "mover dados" como trocas
+        //     ++contagem.qtdTrocas;
+        //     elemento_ordenado -= 1;
+        // }
+
         v[elemento_ordenado + 1] = aux;
         ++contagem.qtdTrocas;
     }
